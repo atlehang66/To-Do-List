e1818694511fa3a22eaf37a20697a712e86ba503
atlehang66 <atlehangsemela@gmail.com>
2024-11-13 14:32:14 +0200

    Server reconfig as well as file directory update

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var http = require('http');  
-http.createServer(function (req, res) {     
-    res.writeHead(200, {'Content-Type': 'text/plain'});     
-    res.end('Hello World!'); 
-}).listen(8080);
+const express = require('express');
+const app = new express();
+app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'path_to_your_views_folder'));
+app.use(express.static('public'));
+app.listen(3000, ()=>{
+    console.log("Server running at http://localhost:3000/");
+});
 
-console.log("Server running at http://localhost:8080/");
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/test', (req, res) => {
+    res.render('test');
+});
+
